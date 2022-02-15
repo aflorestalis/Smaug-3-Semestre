12d359d53d3deb544e00e3fa572d9651a6bb8d6b
Josimar Goncalves <foxchanger@hotmail.com>
2022-02-15 02:22:08 -0300

    Upd

--- Assets/3D Models/Personagem.cs
+++ Assets/3D Models/Personagem.cs
@@ -4,14 +4,27 @@ using UnityEngine;
 
 public class Personagem : MonoBehaviour
 {
+
+    public float moveForce;
+    private Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+       
+    }
+
+    public void Move(Vector2 input)
+    {
+        rb.AddForce(new Vector3(input.x, 0, 0) * moveForce);
+    }
+     void FixedUpdate()
     {
         
     }
